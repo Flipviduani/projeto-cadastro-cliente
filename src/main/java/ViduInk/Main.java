f2787f1c6e02dafcd375b32df633e0e84cf46229
Felipe Viduani <flipviduani@gmail.com>
2026-05-16 15:19:27 -0300

    Versão 1.0
    Projeto de cadastro de clientes com recursos básicos funcionais.

--- src/main/java/ViduInk/Main.java
+++ src/main/java/ViduInk/Main.java
@@ -8,38 +8,7 @@ import java.util.UUID;
 
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-       /*
-       IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
-        */
-
-        /*
-        Para fazer:
-        Sistema para cadastro de clientes
-
-        Funcionalidades:
-        •	Deve permitir que o usuário preencha os dados de um cliente:
-        o	Nome do cliente
-        o	Email
-        o	Telefone
-        o	CPF
-        •	Deve capturar os dados informados e salvar os registros dos clientes em um arquivo de extensão .TXT na máquina do usuário.
-        Este arquivo deverá salvar os seguintes dados:
-        o	ID (identificador único) do cliente
-        o	Nome do cliente
-        o	Email
-        o	Telefone
-        o	CPF
-        o	Data e hora do cadastro
-         */
-
+      
         //Sistema de cadastro de clientes:
 
         //Criando um objeto para usar a Classe Scanner do Java:
@@ -71,8 +40,6 @@ public class Main {
 
         System.out.print("\nE-Mail: ");
         cliente.email = scanner.nextLine();
-
-
 
         cliente.id = UUID.randomUUID(); //Gerando ID do cliente
         cliente.dataHoraCadastro = LocalDateTime.now(); //Gerando a data e hora atuais
